df818bdbfde1c7e71ba0898269440ccb4324b7da
Jason Spencer <j.e.spencer@gmail.com>
2019-01-25 18:06:56 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,99 @@
+#===============================================================================
+# vSphere Provider
+#===============================================================================
+provider "vsphere" {
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"
+  allow_unverified_ssl = "${var.vsphere_unverified_ssl}"
+}
+
+#===============================================================================
+# vSphere Data
+#===============================================================================
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter"
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "${var.vsphere_cluster}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "${var.vsphere_datastore}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "${var.vsphere_port_group}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "${var.vsphere_vm_template}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+#===============================================================================
+# vSphere Resources
+#===============================================================================
+# Create a vSphere VM folder #
+resource "vsphere_folder" "terraform-folder" {
+  path          = "${var.vsphere_vm_folder}"
+  type          = "vm"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  count = "3"
+  name             = "${var.vsphere_vm_name}${count.index + 1}"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  folder           = "${vsphere_folder.terraform-folder.path}"
+
+  # VM resources #
+  num_cpus = "${var.vsphere_vcpu_number}"
+  memory   = "${var.vsphere_memory_size}"
+
+  # Guest OS #
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label             = "disk0"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+    customize {
+      linux_options {
+        host_name = "${var.vsphere_vm_name}"
+        domain    = "${var.vsphere_domain}"
+        time_zone = "${var.vsphere_time_zone}"
+      }
+
+      network_interface {
+     #   ipv4_address = "${var.vsphere_ipv4_address}.${70 + count.index}""
+        ipv4_address = "10.0.0.${70 + count.index}"
+        ipv4_netmask = "${var.vsphere_ipv4_netmask}"
+      }
+
+      ipv4_gateway    = "${var.vsphere_ipv4_gateway}"
+      dns_server_list = ["${var.vsphere_dns_servers}"]
+      dns_suffix_list = ["${var.vsphere_domain}"]
+    }
+
+  }
+
+}
